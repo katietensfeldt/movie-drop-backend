62c10b6d38b9a89f31a84c83b66fcc9f9ea5bb80
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-06-24 16:15:20 -0700

    Reseeded database to make frontend consistent

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(name: "Katie", username: "k10sfeldt", email: "katie@test.com", password: "password")
+User.create(name: "Mike", username: "lordascendant", email: "mike@test.com", password: "password")
+User.create(name: "Dani", username: "daniiscool", email: "dani@test.com", password: "password")
 User.create(name: "John Doe", username: "theunknown", email: "john@test.com", password: "password")
 User.create(name: "Jane", username: "itsjane", email: "jane@test.com", password: "password")
 
