8e928a69b0be9e8baf947bee9da0cd6969e40198
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-06-26 21:08:59 -0700

    Fixes movie index controller for edge cases

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
   def index
     # If user enters a movie title that is multiple words, the api will only work if we add '-' in between each word --- api does not like spaces
-    movie = params[:movie]
+    movie_search = params[:movie]
+    movie = movie_search.split(' ')
+    movie = movie.join("-")
     movies = HTTP.get("http://www.omdbapi.com/?s=#{movie}&type=movie&apikey=#{Rails.application.credentials.omdb_api_key}")
     render json: movies.parse(:json)
   end
