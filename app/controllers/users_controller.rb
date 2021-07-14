e4e4a2c902a5a9b9c9cc6e31a2abba4d21d5f5fa
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-07-14 14:16:15 -0700

    Fixes users index to be searchable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, except: :create
   def index
-    users = User.all
+    users = User.where("username LIKE ?", "%#{params[:search]}%")
     render json: users
   end
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :image, :email, :phone_number
 
   has_many :friends
+  has_many :suggestions
 end
